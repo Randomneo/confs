a1eb4e397474206c63e60cb3805cff68dfc5e1ec
Rostislav Misiura <rostislavm@ronis.mail>
2019-01-31 10:26:29 +0200

    Update install add zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 cp .Xdefaults ~/.Xdefaults
 mkdir -p ~/.emacs.d/conf
 cp base.el ~/.emacs.d/conf/base.el
-sudo cp 00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
 mkdir ~/.config/
+cp .zshrc ~/zshrc
 cp -r awesome ~/.config/awesome
+sudo cp 00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
