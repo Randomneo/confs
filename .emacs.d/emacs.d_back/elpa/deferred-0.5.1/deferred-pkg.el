7c3ae176d8237082ecdca936f23f8a6713829219
Rostislav Misiura <rostislavm@ronis.mail>
2019-02-12 16:08:46 +0200

    emacs

---
+++ .emacs.d/emacs.d_back/elpa/deferred-0.5.1/deferred-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "deferred" "0.5.1" "Simple asynchronous functions for emacs lisp" '((emacs "24.4")))
---
+++ .emacs.d/emacs.d_back/elpa/goto-chg-1.7.2/goto-chg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "goto-chg" "1.7.2" "goto last change" 'nil)
